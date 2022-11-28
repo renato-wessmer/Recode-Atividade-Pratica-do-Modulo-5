a527e92ee5f986db468adbd912e030586ac0465a
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-28 05:45:04 -0300

    Create Cardd.jsx

---
+++ Front-end/AgenciaDeViagensNyssa/src/components/Cards/Cardd.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Cardd.css';
+
+function Cardd() {
+	return (
+		<div className="main1">
+            <ul class="card-wrapper1">
+                <li class="card1">
+                    <img className="img1" src="./src/assets/card1.jpg" alt="" />
+                    <h3><a href="">Uma Super Viagem</a></h3>
+                    <p>Já pensou em fazer aquele voo espacial? Agora imagine um voo na Virgin Galactic, fazer aquela viagem espacial suborbital, nada como ver o planeta de outro perspectiva!</p>
+                </li>
+                <li class="card1">
+                    <img className="img1" src="./src/assets/card2.jpg" alt="" />
+                    <h3><a href="">Urbana ultramoderna que contrasta com comunidades tranquilas</a></h3>
+                    <p>Já pensou em ir ao país conhecido pelas praias tropicais, pelos palácios reais suntuosos, pelas ruínas antigas e pelos templos ornamentados com figuras de Buda? Então a sua vibe é a Tailândia.</p>
+                </li>
+                <li class="card1">
+                    <img className="img1" src="./src/assets/card3.jpg" alt="" />
+                    <h3><a href="">Quer friozinho, chocolate e vinho?</a></h3>
+                    <p>Conhecida por suas paisagens naturais, o friozinho das montanhas, gastronomia rica, muito vinho, tudo isso localizado em Gramado, Canela, Nova Petrópolis,Bento Gonçalves, Garibaldi e Carlos Barbosa.</p>
+                </li>
+                <li class="card1">
+                    <img className="img1" src="./src/assets/card4.jpg" alt="" />
+                    <h3><a href="">Artes, ciência e cultura</a></h3>
+                    <p>São Petersburgo é um centro cultural, com locais como o ultramoderno Teatro Mariinski, com ópera e balé, e o Museu Russo que expõe desde pinturas de ícones ortodoxos até obras de Kandinsky.</p>
+                </li>
+                <li class="card1">
+                    <img className="img1" src="./src/assets/card5.jpg" alt="" />
+                    <h3><a href="">Show natural</a></h3>
+                    <p>País escandinavo, abrange montanhas, geleiras e fiordes litorâneos profundos, existem navios vikings preservados do século IX, e sua aurora boreal, é de tirar o folêgo, essa é a Noruega.</p>
+                </li>
+                <li class="card1">
+                    <img className="img1" src="./src/assets/card6.jpg" alt="" />
+                    <h3><a href="">Uma inesquecível aventura</a></h3>
+                    <p>Dunas, praias lindas com mar morninho e sol garantido praticamente o ano todo, a culinária da cidade é recheada a fartura de camarões, passeios de bugue pelas dunas de Genipabu, essa é Natal.</p>
+                </li>
+                <li class="card1">
+                    <img className="img1" src="./src/assets/card7.jpg" alt="" />
+                    <h3><a href="">Isso que é vida</a></h3>
+                    <p>Conhecida como "Ilha da Magia", paradisíacas, ótimos picos de surf, lagoas, dunas, trilhas, construções coloniais, sítios arqueológicos e gastronomia deliciosa, essa é Florianópolis.</p>
+                </li>
+                <li class="card1">
+                    <img className="img1" src="./src/assets/card8.jpg" alt="" />
+                    <h3><a href="">A moderna, tradição e modernidade</a></h3>
+                    <p>A cidade que nasceu do cultivo de pérolas, hoje é grande centro de negócios, lojas de luxo, tradição com os mercados de ouro e muitas atrações para os visitantes. Essa é Dubai.</p>
+                </li>
+                <li class="card1">
+                    <img className="img1" src="./src/assets/card9.jpg" alt="" />
+                    <h3><a href="">Bonito só se for em Mato Grosso do Sul</a></h3>
+                    <p>Rios de águas cristalinas, grutas, fauna e flora preservadas e considerado um dos principais destinos de ecoturismo do Brasil.</p>
+                </li>
+                <li class="card1">
+                </li>
+            </ul>
+		</div>
+	)
+}
+
+export default Cardd
