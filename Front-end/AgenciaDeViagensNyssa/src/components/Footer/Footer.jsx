2ac4cf0201bc23f252691a5781922fdf9eb820b4
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-28 05:45:36 -0300

    Create Footer.jsx

---
+++ Front-end/AgenciaDeViagensNyssa/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+	return (
+		<div>
+			<footer>
+				<p>&copy; - Nyssa Inc, (Nyssa Brasil Operatoro kaj Vojaĝagentejo S.A)</p>
+			</footer>
+		</div>
+	)
+}
+
+export default Footer
