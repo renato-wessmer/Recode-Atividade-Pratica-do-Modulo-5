aa48a526cd043891c3dcef5a8d107c34c53a8a0c
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-28 05:45:34 -0300

    Create Home.jsx

---
+++ Front-end/AgenciaDeViagensNyssa/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Type from '../../components/Writer/Writer';
+
+import './Home.css'
+
+const Home = () => {
+	return (
+		<div className="bg">
+			<Type></Type>
+		</div>
+	)
+}
+
+export default Home
