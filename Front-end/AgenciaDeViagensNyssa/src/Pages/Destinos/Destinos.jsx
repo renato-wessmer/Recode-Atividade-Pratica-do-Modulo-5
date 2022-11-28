d4872984d60960e813b32acc32e332cd16053165
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-28 05:45:28 -0300

    Create Destinos.jsx

---
+++ Front-end/AgenciaDeViagensNyssa/src/Pages/Destinos/Destinos.jsx
@@ -0,0 +1,13 @@
+import Cards2 from './../../components/Cards/Cardd';
+
+import './../../components/Cards/Cardd.css'
+
+const Locais = () => {
+	return (
+		<div className="bg">
+			<Cards2></Cards2>
+		</div>
+	)
+}
+
+export default Locais
